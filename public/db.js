eeae296923fd8f0c5f7a736281caabecb14fc2cc
Chris Burns <ckbzxz@gmail.com>
2021-02-20 10:58:44 -0500

    finised check database function

--- public/db.js
+++ public/db.js
@@ -40,4 +40,24 @@ const checkDatabase = () => {
     const store = transaction.objectStore("pending");
     //get all records and store in const
     const getAll = store.getAll();
+
+    getAll.onsuccess = () => {
+        if(getAll.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json"
+                }
+            })
+            .then(response => response.json())
+            .then(() => {
+                const transaction = db.transaction(["pending"], "readwrite")
+                const store = transaction.objectStore("pending");
+                store.clear();
+            })
+        }
+    }
 }
+window.addEventListener("online", checkDatabase);
